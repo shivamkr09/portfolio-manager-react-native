5a15b4259c165f1740fbcd98f23a1c32d73f6adf
shivamkr09 <shivamkumarroy98@gmail.com>
2025-03-14 03:24:09 +0000

    redux configured

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider } from 'react-redux';
 import AppNavigator from './src/navigation/AppNavigator';
+import { store } from './src/store';
 
 export default function App() {
   return (
-    <PaperProvider>
-      <AppNavigator />
-      <StatusBar style="auto" />
-    </PaperProvider>
+    <Provider store={store}>
+      <PaperProvider>
+        <AppNavigator />
+        <StatusBar style="auto" />
+      </PaperProvider>
+    </Provider>
   );
 }
 
